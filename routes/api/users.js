ff1011271a780ba5ecaab5e14b761613510c1abd
jomarcui <cuijomar@gmail.com>
2022-08-11 23:32:48 +0800

    🔥 Removed commented codes

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,13 +17,4 @@ router
 
 router.route('/:id').get(getUserById);
 
-// router.route('/auth').post(async (req, res) => {
-//   await users
-//     .findOne({ email: req.body.email, password: req.body.password })
-//     .then((user) => {
-//       return res.json(user);
-//     })
-//     .catch((err) => res.status(400).json(`Error: ${err}`));
-// });
-
 module.exports = router;
